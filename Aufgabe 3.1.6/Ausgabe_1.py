8a2f4f72a284633bfa20c3f70c52402e7c386f5e
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-05-23 10:10:12 +0000

    Ausgabe_1 und Ausgabe_2 hinzugefügt

---
+++ Aufgabe 3.1.6/Ausgabe_1.py
@@ -0,0 +1,14 @@
+name = "Max Mustermann"
+age = 30
+gender = "männlich"
+height = "1,80m"
+weight = "75kg"
+eye_color = "braun"
+hair_color = "schwarz"
+occupation = "Softwareentwickler"
+hobby = "Lesen"
+trait = "freundlich"
+
+print(f"""Mein Name ist {name}. Ich bin {age} Jahre alt, bin {height} groß und wiege {weight}. 
+Meine Augenfarbe ist {eye_color}, meine Haare sind {hair_color}. Ich bin {occupation}, liebe {hobby} und bin {trait}""")
+
---
+++ Aufgabe 3.1.6/Ausgabe_2.py
@@ -0,0 +1,11 @@
+name = "Sascha"
+location ="zuhause"
+place = "Zimmer"
+spot = "Laptop" 
+task_1 = "Python"
+mood = "Freude"
+task_2 = "Programmieren"
+action = "lernen"
+
+print(f"""{name} sitzt {location} in seinem {place} und arbeitet am {spot}.
+Er arbeitet mit {task_1} und es macht ihm große {mood} etwas über das {task_2} zu {action}.""")
