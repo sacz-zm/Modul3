8bd9478b774e95db3baf4f9d0848ed4bb4d82a2e
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-05-23 09:28:54 +0000

    Aufgabe 3.1.5 hinzugefügt

---
+++ Aufgabe 3.15/Fahrzeug.py
@@ -0,0 +1,13 @@
+color = "blau"
+size = "groß"
+doors = 3
+fuel = "Diesel"
+type = "LKW"
+brand = "Mercedes"
+tyres = 6
+seats = 3
+condition = "new"
+gear_box = "manuell"
+print(locals())
+print("doors", tyres)
+print("tyres", doors)
