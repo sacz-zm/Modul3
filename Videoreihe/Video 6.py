716146457cf3a84d5f08e6bef9565ce335ef99e2
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-05-26 07:50:43 +0000

    Videoreihe - Video 6 hinzugefügt

---
+++ Videoreihe/Video 6.py
@@ -0,0 +1,8 @@
+print("Anfang des Programmes")
+
+name = input("Bitte deinen Namen eingeben: ")
+print("Mein Name ist " + name)
+age = input("Bitte dein aktuelles Alter eingeben: ")
+print("Mein Alter ist " + age)
+
+print("Ende des Programmes")
