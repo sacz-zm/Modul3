686d6ae7856f31ab3aacb170218366a813c1c83c
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-05-26 08:30:39 +0000

    Aufgabe 3.2.1 hinzugefügt

---
+++ Aufgabe 3.2.1/Zimmer.py
@@ -0,0 +1,41 @@
+print("Das Zimmer")
+
+print("Wie viele Wände hat das Zimmer?")
+walls = input("Anzahl Wände: ")
+print(f"Das Zimmer hat {walls} Wände")
+
+print("Wie Groß ist das Zimmer?")
+size = input("Quadratmeter: ")
+print(f"Das Zimmer hat {size}m^2")
+
+print("Welche Farbe haben die Wände?")
+color = input("Farbe: " )
+print("Die Wände sind " + color)
+
+print("Wie viele Fenster hat das Zimmer?")
+window = input("Anzahl: ")
+print(f"Das Zimmer hat {window} Fenster")
+
+print("Welche Farbe hat der Schreibtisch?")
+color_desk = input("Farbe: ")
+print(f"Der Schreibtisch ist {color_desk}")
+
+print("Wie lang ist der Schreibtisch?")
+size_desk = input("Länge in Metern: ")
+print(f"Der Schreibtisch ist {size_desk}m groß")
+
+print("Ist der Kleiderschrank offen oder geschlossen?")
+state = input("Antwort: ")
+print(f"Der Kleiderschrank ist {state}")
+
+print("Was für einen Boden hat das Zimmer?")
+floor = input("Bodentyp: ")
+print(f"Im Zimmer ist {floor} verlegt")
+
+print("Was befindet sich noch im Zimmer?")
+object = input("Gegenstand: ")
+print(f"Im Zimmer befindet sich ein/e {object}")
+
+print("Was für ein Zimmer ist es?")
+type = input("Typ: ")
+print(f"Es handelt sich um ein {type}")
