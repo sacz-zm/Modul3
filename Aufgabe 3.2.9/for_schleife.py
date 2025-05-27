a24282a0c1acb1a4ab4615eb8b7cdfc01af756e4
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-05-27 13:15:55 +0000

    3.2.9 hinzugefügt

---
+++ Aufgabe 3.2.9/for_schleife.py
@@ -0,0 +1,8 @@
+
+
+for element in "Ich bin ein String": # Schleifenkopf Name "element" ist frei wählbar
+    print(element)           # Schleifenkörper
+
+########################################################################################
+# Lässt Iterierbare Objekte durchlaufen z.B. Listen, Strings
+# Dient als Zählerschleife
---
+++ Aufgabe 3.2.9/zählerschleife.py
@@ -0,0 +1,11 @@
+
+
+for element in range(5, 10, 2): 
+    print(element)
+
+#######################################################################################
+# range() erstellt ein iterierbares Element von 0 - 10 (10 ist exkludiert)
+# range(x, x) erstellt ein Element mit Start x (inkludiert) und Ende x (exkludiert)
+# Dritte Zahl gibt den Schrittwert an (Standard bei 1)
+# Kann genutzt werden, wenn man eine Wiederholung eines bestimmten Codeblocks nicht von einer Bedingung abhängig machen möchte
+# Um eine Anzahl von Wiederholungen fest vorzugeben
