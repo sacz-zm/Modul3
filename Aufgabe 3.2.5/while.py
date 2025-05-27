e69cca6d8fb5884aae79729a0c8b96572d66532b
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-05-27 08:59:25 +0000

    3.2.5 hinzugefügt

---
+++ Aufgabe 3.2.5/while.py
@@ -0,0 +1,11 @@
+
+
+counter = 5
+while counter < 10: # Schleifenkopf - Bedingung muss true oder false sein
+    print("Hier steht Coder der wiederholt ausgeführt werden soll") # Schleifenkörper
+    counter += 1 # counter wird nach einem Durchlauf um 1 erhöh
+
+
+###################################################################
+# Bedingung entscheided wie oft die Schleife ausgeführt wird
+# Wenn Schleifenkörper == false, wird die Schleife beendet
