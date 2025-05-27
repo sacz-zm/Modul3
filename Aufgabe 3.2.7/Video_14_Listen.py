aefb6f126ce263e5087cda986161c6ba299f0383
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-05-27 11:23:30 +0000

    3.2.7 hinzugefügt

---
+++ Aufgabe 3.2.7/Video_14_Listen.py
@@ -0,0 +1,13 @@
+
+
+numbers = [3, 5, 15, 17, 20]
+print(type(numbers))
+
+names = ["Hans", "Franz", "Manz", "Lanz", 4, 8.2]
+print(names)
+
+############################################
+# Listen können mehrere Werte speichern
+# [] Eckige Klammern
+# Datentyp: list
+# Kann jede Art von Datentyp speichern
---
+++ Aufgabe 3.2.7/Video_15_Zugriff.py
@@ -0,0 +1,10 @@
+
+
+names = ["Hans", "Franz", "Manz", "Lanz", 4, 8.2]
+print(names)
+
+print(names[3]) # [] + Index, nach dem Bezeichner = Zugriff auf ein bestimmtes Element der Liste | Index startet bei 0 
+                # Mit negativem Index wird die Liste von hinten abgefragt | Fängt bei -1 an
+
+names[0] = "Panz" # Ändert das bestimmte Element aus der Liste
+
