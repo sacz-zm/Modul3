7bdbeed966f5ea3f7909c7b5a66911525a39de19
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-05-28 20:04:40 +0000

    code angepasst

--- Aufgabe 3.2.14/video_24_methoden_klassen.py
+++ Aufgabe 3.2.14/video_24_methoden_klassen.py
@@ -17,6 +17,6 @@ class Car:
 car_1 = Car() 
 print(car_1.x_position) # Gibt anfangs x Koordinate des Objektes car_1 aus 
 print(car_1.y_position) # Gibt anfangs y Koordinate des Objektes car_1 aus
-car_1.drive(5, 10) # Veränderung der Koordinaten (self.y_position += x) | x Koordinate wird um 5 erhöht | y Koordinate wird um 10 erhöht
+car_1.drive(5, 10) # Veränderung der Koordinaten self.x_position += x und self.x_position += y | x Koordinate wird um 5 erhöht | y Koordinate wird um 10 erhöht
 print(car_1.x_position) # Gibt neue x Koordinate des Objektes aus
 print(car_1.y_position) # Gibt neue y Koordinate des Objektes aus
