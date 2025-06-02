3553a796e801489cf7f49f87d064b16aff7f74c4
sacz-zm <sascha.czoske@schule-zukunftsmotor.org>
2025-06-02 09:07:34 +0000

    code angepasst

--- Aufgabe 3.3.01/bankkonto.py
+++ Aufgabe 3.3.01/bankkonto.py
@@ -33,11 +33,3 @@ class BankAccount:
             zinsen = (self.kontostand / 100) * zinssatz
             self.kontostand += zinsen   # Zinsen werden gutgeschrieben
 
-print("Test: Einzahlen + Zinsen:")
-acc1 = BankAccount("Max Mustermann")
-acc1.einzahlen(100)
-acc1.zinsen_berechnen(2)
-print(acc1)
-
-print("\nTest: Konto überziehen:")
-acc1.abheben(203)
--- Aufgabe 3.3.01/main.py
+++ Aufgabe 3.3.01/main.py
@@ -1,2 +1,11 @@
 
-import bankkonto
+from bankkonto import BankAccount
+
+print("Test: Einzahlen + Zinsen:")
+acc1 = BankAccount("Max Mustermann")
+acc1.einzahlen(100)
+acc1.zinsen_berechnen(2)
+print(acc1)
+
+print("\nTest: Konto überziehen:")
+acc1.abheben(203)
